8b8b0c74d964fedba48a41661085611bf7eadd02
SWEETPOT4T0 <lisk1223@gmail.com>
2024-12-10 17:49:48 +0900

    Baekjoon

--- src/Baekjoon/Bj10807.java
+++ src/Baekjoon/Bj10807.java
@@ -1,18 +1,25 @@
-package Baekjoon;
-
-import java.util.Scanner;
-/*
-ublic class Bj10807 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-
-        int[] arr = sc.nextInt([])
-
-        for (int i = 0; i < N; i++) {
-
-        }
-    }
-}
-*/
+//package Baekjoon;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.util.Scanner;
+//
+//public class Bj10807 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int N = Integer.parseInt(br.readLine());
+//        int count = 0;
+//
+//
+//        for (int i = 0; i < N; i++) {
+//            String[] inputs = (br.readLine().split(" "));
+//            int v = Integer.parseInt(br.readLine());
+//            if (inputs[i] == v) {
+//                count++;
+//            }
+//
+//        }
+//        System.out.println(count);
+//    }
+//}
